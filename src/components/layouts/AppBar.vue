1c9dec20ecab24d7523b24402b52a1b31685ac5a
berbraga <bernardo.sbraga@yahoo.com>
2023-05-18 15:44:04 -0300

    iniciando o projeto do prontuario eletronico

---
+++ src/components/layouts/AppBar.vue
@@ -0,0 +1,78 @@
+<template>
+	<!-- <v-toolbar color="green"  >
+        <v-app-bar-nav-icon></v-app-bar-nav-icon>
+        <v-spacer></v-spacer>
+      </v-toolbar> -->
+	<v-app-bar color="green" prominent>
+		<!-- <v-app-bar-nav-icon variant="text" @click.stop="drawer = !drawer"></v-app-bar-nav-icon> -->
+		<img
+			width="150"
+			aspect-ratio="1/1"
+			src="@/assets/logos/Logo-UHC---Preferencial.png"
+		/>
+
+		<v-spacer></v-spacer>
+	</v-app-bar>
+
+	<v-navigation-drawer
+		v-model="drawer"
+		:rail="rail"
+		permanent
+		@click="rail = false"
+	>
+		<v-list-item
+			prepend-avatar="https://randomuser.me/api/portraits/men/85.jpg"
+			title="John Leider"
+			nav
+		>
+			<template v-slot:append>
+				<v-btn
+					variant="text"
+					icon="mdi-chevron-left"
+					@click.stop="rail = !rail"
+				></v-btn>
+			</template>
+		</v-list-item>
+
+		<v-divider></v-divider>
+
+		<v-list density="compact" nav>
+			<v-list-item
+				prepend-icon="mdi-home-city"
+				title="Home"
+				value="home"
+			></v-list-item>
+			<v-list-item
+				prepend-icon="mdi-account"
+				title="My Account"
+				value="account"
+			></v-list-item>
+			<v-list-item
+				prepend-icon="mdi-account-group-outline"
+				title="Users"
+				value="users"
+			></v-list-item>
+		</v-list>
+	</v-navigation-drawer>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			drawer: true,
+			items: [
+				{ title: "Home", icon: "mdi-home-city" },
+				{ title: "My Account", icon: "mdi-account" },
+				{ title: "Users", icon: "mdi-account-group-outline" },
+			],
+			rail: true,
+		};
+	},
+	watch: {
+		group() {
+			this.drawer = false;
+		},
+	},
+};
+</script>
---
+++ src/layouts/default/Default.vue
@@ -0,0 +1,11 @@
+<template>
+	<v-app>
+		<app-bar />
+		<default-view />
+	</v-app>
+</template>
+
+<script setup>
+import DefaultView from "@/components/layouts/View.vue";
+import AppBar from "@/components/layouts/AppBar.vue";
+</script>
