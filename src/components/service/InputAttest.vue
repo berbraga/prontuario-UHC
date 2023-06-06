a79f01022d525effc273ff0ac2c92f80c4081d90
berbraga <bernardo.sbraga@yahoo.com>
2023-06-06 16:49:16 -0300

    tentando fazer o wysiwyg funcionar

--- src/components/service/InputAttest.vue
+++ src/components/service/InputAttest.vue
@@ -8,10 +8,7 @@
 				<v-btn color="primary" class="ml-3">imprimir</v-btn>
 			</div>
 		</div>
-		<!-- <vue-quill-editor
-		v-model="contentHtml">
 
-		</vue-quill-editor> -->
 		<v-textarea
 			label="Modelo de Atestado"
 			class="ma-0"
@@ -23,8 +20,55 @@
 
 <script setup>
 	import DateToday from '../card/DateToday.vue';
-	import VueQuillEditor from "vue-quill-editor";
-	import { ref } from 'vue';
-	const contentHtml = ref('');
 </script>
 
+
+
+<!-- <template>
+  <div>
+
+    <tiptap-vuetify
+      v-model="content"
+      :extensions="extensions"
+    />
+  </div>
+</template>
+
+<script>
+// import the component and the necessary extensions
+import { TiptapVuetify, Heading, Bold, Italic, Strike, Underline, Code, Paragraph, BulletList, OrderedList, ListItem, Link, Blockquote, HardBreak, HorizontalRule, History } from 'tiptap-vuetify'
+
+export default {
+  // specify TiptapVuetify component in "components"
+  components: { TiptapVuetify },
+  data: () => ({
+    // declare extensions you want to use
+    extensions: [
+      History,
+      Blockquote,
+      Link,
+      Underline,
+      Strike,
+      Italic,
+      ListItem,
+      BulletList,
+      OrderedList,
+      [Heading, {
+        options: {
+          levels: [1, 2, 3]
+        }
+      }],
+      Bold,
+      Code,
+      HorizontalRule,
+      Paragraph,
+      HardBreak
+    ],
+    // starting editor's content
+    content: `
+      <h1>Yay Headlines!</h1>
+      <p>All these <strong>cool tags</strong> are working now.</p>
+    `
+  })
+}
+</script> -->
