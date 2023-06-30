4f0243f571a470e2e6dbe39748b72481ab1c69be
berbraga <bernardo.sbraga@yahoo.com>
2023-06-30 15:24:40 -0300

    a

---
+++ src/store/firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBDpkH1kawQ2wdnsrmZ7oDd0RGoxjRI4Qo",
+  authDomain: "ultracarebr.firebaseapp.com",
+  databaseURL: "https://ultracarebr.firebaseio.com",
+  projectId: "ultracarebr",
+  storageBucket: "ultracarebr.appspot.com",
+  messagingSenderId: "197582994272",
+  appId: "1:197582994272:web:526b1daf4360bb66b43c05"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
